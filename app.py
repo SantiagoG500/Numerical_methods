d929194b4a0cbe961eeb2bac6295d8c1a8f04ef9
Santiago Gamboa <santiagogamboa.b500@gmail.com>
2026-05-10 17:47:36 -0500

    feat: add jacobi and gause-seidel methods into gradio

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 import gradio as gr
-from methods.non_linear.Biseccion import biseccion_metodo 
+from methods.non_linear.biseccion import biseccion_metodo 
 from methods.non_linear.falsa_posicion import falsa_posicion_metodo
-from methods.non_linear.Newton_rapson import newton_raphson_metodo
+from methods.non_linear.newton_rapson import newton_raphson_metodo
 from methods.non_linear.punto_fijo import punto_fijo_metodo
 from methods.non_linear.secante import secante_metodo
+from methods.linear.gauss_seidel import gauss_seidel_metodo
+from methods.linear.jacobi import jacobi_metodo
+
+def update_matrix_inputs(n):
+    n = int(n)
+    headers_a = [f"A[{i+1}]" for i in range(n)]
+    
+    return [
+        gr.update(
+            headers=headers_a, 
+            row_count=n, 
+            column_count=n, 
+            value=[[0]*n for _ in range(n)] # Opcional: resetea a ceros para evitar conflictos
+        ),
+        gr.update(
+            row_count=n, 
+            column_count=1, 
+            value=[[0] for _ in range(n)]
+        )
+    ]
+    
 
 with gr.Blocks(title="Calculadora de métodos numéricos") as demo:
     gr.Markdown("## Método de Bisección")
@@ -101,6 +122,59 @@ with gr.Blocks(title="Calculadora de métodos numéricos") as demo:
             inputs=[secante_fx_input, secante_x0_input, secante_x1_input, secante_tol_input, secante_max_input],
             outputs=[res_output, table_output]
         )
+
+    with gr.Tab('Gauss-Seidel'):
+        with gr.Column():
+            gauss_d_input = gr.Slider(label="Dimensión del sistema", minimum=2, maximum=10, step=1, value=3)
+            gauss_a_input = gr.Dataframe(label="Matriz A", headers=["A[1]", "A[2]", "A[3]"], row_count=3, column_count=3, datatype="number", interactive=True, type="array")
+            gauss_b_input = gr.Dataframe(label="Vector b", headers=["b"], row_count=3, column_count=1, datatype="number", interactive=True, type="array")
+            gauss_tol_input = gr.Number(label="Tolerancia", value=1e-6, precision=10)
+            gauss_iter_input = gr.Number(label="Máximo de iteraciones", value=100, precision=0)
+
+            run_gauss = gr.Button("Calcular Gauss-Seidel")
+
+        with gr.Column():
+            res_output = gr.Textbox(label="Resultados", interactive=False)
+            res_2_output = gr.Textbox(label="Resultados 2", interactive=False)
+
+        gauss_d_input.change(
+            fn=update_matrix_inputs,
+            inputs=gauss_d_input,
+            outputs=[gauss_a_input, gauss_b_input]
+        )
+        
+        run_gauss.click(
+            gauss_seidel_metodo,
+            inputs=[gauss_d_input, gauss_a_input, gauss_b_input, gauss_tol_input, gauss_iter_input],
+            outputs=[res_output, res_2_output]
+        )
+    
+    with gr.Tab('Jacobi'):
+        with gr.Column():
+            jacobi_d_input = gr.Slider(label="Dimensión del sistema", minimum=2, maximum=10, step=1, value=3)
+            jacobi_a_input = gr.Dataframe(label="Matriz A", headers=["A[1]", "A[2]", "A[3]"], row_count=3, column_count=3, datatype="number", interactive=True, type="array")
+            jacobi_b_input = gr.Dataframe(label="Vector b", headers=["b"], row_count=3, column_count=1, datatype="number", interactive=True, type="array")
+            jacobi_tol_input = gr.Number(label="Tolerancia", value=1e-6, precision=10)
+            jacobi_iter_input = gr.Number(label="Máximo de iteraciones", value=100, precision=0)
+
+            run_jacobi = gr.Button("Calcular Jacobi")
+        with gr.Column():
+            jacobi_res_output = gr.Textbox(label="Resultados", interactive=False)
+            jacobi_res_2_output = gr.Textbox(label="Resultados 2", interactive=False)
+            
+        jacobi_d_input.change(
+            fn=update_matrix_inputs,
+            inputs=jacobi_d_input,
+            outputs=[jacobi_a_input, jacobi_b_input]
+        )
+           
+        run_jacobi.click(
+            jacobi_metodo,
+            inputs=[jacobi_d_input, jacobi_a_input, jacobi_b_input, jacobi_tol_input, jacobi_iter_input],
+            outputs=[jacobi_res_output, jacobi_res_2_output]
+        )     
+        
+
 
 # Lanzamos la app
 if __name__ == "__main__":
