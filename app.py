a56eb1ce4f06534ef297e87cc62392dd9644a1cb
Santiago G. <89423075+SantiagoG500@users.noreply.github.com>
2026-05-10 20:14:32 -0500

    Launch app with specific server settings

--- app.py
+++ app.py
@@ -174,6 +174,5 @@ with gr.Blocks(title="Calculadora de métodos numéricos") as demo:
         
 
 
-# Lanzamos la app
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860)
