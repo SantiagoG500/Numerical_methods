17b07f29f5313db825955e9a6b265269f411e593
Santiago G. <89423075+SantiagoG500@users.noreply.github.com>
2026-05-10 20:22:58 -0500

    Simplify demo launch configuration
    Removed server name and port from demo launch.

--- app.py
+++ app.py
@@ -173,6 +173,5 @@ with gr.Blocks(title="Calculadora de métodos numéricos") as demo:
         )     
         
 
-
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    demo.launch()
